542a3e2946347c591272ca929d2050ab66947ab4
hhappa0 <156544000+hhappa0-code@users.noreply.github.com>
2026-02-19 16:59:12 +0100

    Highlight requested rating & update to 2.2074

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,22 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/RateStarsLayer.hpp>
 
 using namespace geode::prelude;
+
+// Colors rating button green which level creator has requested
+class $modify(RateStarsLayer)
+{
+    $override
+    bool init(int levelID, bool platformer, bool moderator) {
+        if (!RateStarsLayer::init(levelID, platformer, moderator)) return false;
+
+        auto level = GameLevelManager::get()->getSavedLevel(m_levelID);
+        int requested = level->m_starsRequested;
+
+        if (requested <= 0 || requested > 10) return true;
+
+        m_mainLayer->getChildByType<CCMenu>(0)->getChildByType<CCMenuItemSpriteExtra>(requested - 1)->getChildByType<ButtonSprite>(0)->setColor(ccColor3B(0, 255, 0));
+
+        return true;
+    }
+};
