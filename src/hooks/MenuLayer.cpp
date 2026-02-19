ede313310e3c62d909ecb034e4d32f7beabb041f
hhappa0 <156544000+hhappa0-code@users.noreply.github.com>
2026-02-19 22:05:50 +0100

    Reformat source

---
+++ src/hooks/MenuLayer.cpp
@@ -0,0 +1,68 @@
+#include <Geode/modify/MenuLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(HHGDMenuLayer, MenuLayer)
+{
+    bool init() {
+        if (!MenuLayer::init())
+        {
+            return false;
+        }
+
+        auto closeMenu = static_cast<CCMenu*>(this
+            ->getChildByID("close-menu"));
+        auto rightSideMenu = static_cast<CCMenu*>(this
+            ->getChildByID("right-side-menu"));
+
+        auto closeButtonSprite = CCSprite::create("HHGD_closeBtn_001.png"_spr);
+        closeButtonSprite
+            ->setScale(0.7f);
+        auto restartButtonSprite = CCSprite::createWithSpriteFrameName("GJ_updateBtn_001.png");
+        restartButtonSprite
+            ->setScale(0.7f);
+        auto hhgdButtonSprite = ButtonSprite::create("HHGD");
+        hhgdButtonSprite
+            ->setScale(0.7f);
+
+        auto closeButton = static_cast<CCMenuItemSpriteExtra*>(closeMenu
+            ->getChildByID("close-button"));
+        auto restartButton = CCMenuItemSpriteExtra::create(restartButtonSprite, this, menu_selector(HHGDMenuLayer::onRestartButtonClick));
+        auto hhgdButton = CCMenuItemSpriteExtra::create(hhgdButtonSprite, this, menu_selector(HHGDMenuLayer::onHHGDButtonClick));
+
+        closeButton
+            ->setSprite(closeButtonSprite);
+        restartButton
+            ->setID("restart-button"_spr);
+        closeMenu
+            ->addChild(restartButton);
+
+        hhgdButton
+            ->setID("hhgd-button"_spr);
+        rightSideMenu
+            ->addChild(hhgdButton);
+        
+        closeMenu
+            ->updateLayout();
+        rightSideMenu
+            ->updateLayout();
+
+        return true;
+    }
+
+    void onRestartButtonClick(CCObject* sender)
+    {
+        createQuickPopup("Restart Game", "Are you sure you want to <cy>restart</c>?", "Cancel", "Yes", [](auto, bool yesButton)
+        {
+            if (yesButton)
+            {
+                geode::utils::game::restart(true);
+            }
+        });
+    }
+
+    void onHHGDButtonClick(CCObject* sender)
+    {
+
+    }
+};
---
+++ src/hooks/RateStarsLayer.cpp
@@ -0,0 +1,33 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/RateStarsLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(HHGDRateStarsLayer, RateStarsLayer)
+{
+    $override
+    bool init(int levelID, bool platformer, bool moderator) {
+        if (!RateStarsLayer::init(levelID, platformer, moderator))
+        {
+            return false;
+        }
+
+        auto level = GameLevelManager::get()
+            ->getSavedLevel(m_levelID);
+        int requested = level
+            ->m_starsRequested;
+
+        if (requested <= 0 || requested > 10)
+        {
+            return true;
+        }
+
+        m_mainLayer
+            ->getChildByType<CCMenu>(0)
+            ->getChildByType<CCMenuItemSpriteExtra>(requested - 1)
+            ->getChildByType<ButtonSprite>(0)
+            ->setColor(ccColor3B(0, 255, 0));
+
+        return true;
+    }
+};
